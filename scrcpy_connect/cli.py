9edaecb41a521f829a07ed3b529cf55c1e09fd7c
mateuspim <mateuspimsantos@gmail.com>
2025-07-16 23:38:26 -0300

    Add logging functionality

--- scrcpy_connect/cli.py
+++ scrcpy_connect/cli.py
@@ -1,6 +1,13 @@
+from scrcpy_connect.logger import setup_logging
+
+setup_logging()
+
 import argparse
+import logging
+logger = logging.getLogger(__name__)
 
 def main():
+    logger.info("Starting scrcpy-connect CLI")
     parser = argparse.ArgumentParser(
         description="Mirror android screen over WiFi using adb & scrcpy."
     )
