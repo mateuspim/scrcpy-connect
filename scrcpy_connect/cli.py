814e0e510d39caa00c85a78c0c277d19390bcc74
mateuspim <mateuspimsantos@gmail.com>
2025-07-16 23:08:21 -0300

    Add a simple CLI main function

--- scrcpy_connect/cli.py
+++ scrcpy_connect/cli.py
@@ -1,5 +1,27 @@
+import argparse
+
 def main():
-    raise NotImplementedError
+    parser = argparse.ArgumentParser(
+        description="Mirror android screen over WiFi using adb & scrcpy."
+    )
+    parser.add_argument(
+        "--ip",
+        required=False,
+        help="IP address of the Android device"
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=5555,
+        help="Port number (default: 5555)"
+    )
+    parser.add_argument(
+        "adb_args",
+        nargs=argparse.REMAINDER,
+        help="Additional arguments to pass to adb"
+    )
+    args = parser.parse_args()
+
 
 if __name__ == "__main__":
     main()
